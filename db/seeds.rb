f5796e2f7c66c73be30c4b7e4bbbd37ad900a76d
Federico Luna <fluna1973@yahoo.com>
2023-01-03 14:25:07 -0600

    Initialize endpoints

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: "Welcome Micronaut!")
+Greeting.create(message: "Hello world!")
+Greeting.create(message: "Hello World of rails webpack!")
+Greeting.create(message: "Nice to have You back")
+Greeting.create(message: "Hello final Capstone coders")
